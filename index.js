ab03a2d0ecdd87d127fd6c56b2aa188de87f8342
Hristo Kutsarov <103206944+Hr-kutsarov@users.noreply.github.com>
2023-03-23 13:30:54 +0200

    Added extra pages to the article.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // alert("hello");
+let shortBioTextBox = document.getElementById("short-bio-text");
+let articleTitle = document.querySelector("#short-bio-text > h2");
+let techButton = document.getElementById("technologies");
+let articleParagraph = document.querySelector(".article-text-front");
+
+// there are 2 paragraphs, need a mapping function
+let helloBtn = document.getElementById("hello-btn");
+
+const additionalInfo =
+  "Laudantium odio placeat magnam minima molestiae saepe tempore qui explicabo pariatur hic? Alias dignissimos expedita dicta ex nam nobis consectetur distinctio officia quisquam assumenda, nulla aspernatur placeat dolores necessitatibus tenetur voluptatum labore accusantium deleniti nihil minima enim voluptates praesentium! Magni, dolorum error. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aspernatur nihil adipisci modi rem voluptatem, ullam nobis veritatis officiis nostrum recusandae rerum dicta nisi obcaecati corporis sunt molestias unde ea.";
+
+const additionalInfoParagraph =
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, eos? Ipsum earum voluptatem illo harum, repudiandae, a ipsam.";
+// GLOBAL
+
+function createNewParagraphElement(text) {
+  let newParagraph = document.createElement("p");
+  newParagraph.classList.add("article-text-front");
+  newParagraph.textContent = text;
+  return newParagraph;
+}
+
+function pageRefresh() {
+  location.reload();
+  return;
+}
+// ARTICLE ARTICLE ARTICLE ARTICLE ARTICLE ARTICLE ARTICLE ARTICLE //
+
+function toggleBio() {
+  articleTitle.textContent = "Bio";
+  articleParagraph.textContent = additionalInfo;
+  helloBtn.textContent = "My interests";
+
+  let newP = createNewParagraphElement(additionalInfo);
+  shortBioTextBox.appendChild(newP);
+  helloBtn.style.display = "none";
+  techButton.style.display = "block";
+}
+
+function toggleTechnologies() {
+  articleTitle.textContent = "Technologies";
+  articleParagraph.textContent = additionalInfo;
+  let newP = createNewParagraphElement(additionalInfoParagraph);
+  shortBioTextBox.appendChild(newP);
+
+  techButton.textContent = "Return";
+  techButton.addEventListener("click", pageRefresh);
+}
+
+// ======================== EVENT LISTENTERS ======================//
+helloBtn.addEventListener("click", toggleBio);
+techButton.addEventListener("click", toggleTechnologies);
